5159ee3abee0075591b01cbc742e12d3c4e44f7f
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-04-21 11:50:35 +0200

    Updated Experiment #2

--- scripts/score_vs_R_k.sh
+++ scripts/score_vs_R_k.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
 
-days=7
 methods=(individual dorfman negbin)
-lambda_1=0.3
-lambda_2=0.2
 se=0.7
 sp=0.95
+N_seq=(20 100)
 r_seq=(0.5 1.0 1.5 2.0 2.5 3.0)
-k_seq=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.5 0.6 1.0 10.0 100.0 1000.0 10000.0)
+k_seq=(0.1 0.2 0.3 0.4 0.5 1.0 1000.0 1000000.0 1000000000.0 1000000000000.0)
 seeds=1000
 njobs=40
 
-for z in {0..5}
+# Optimizing FNs
+lambda_1=1.0
+lambda_2=0.0
+for v in {0..1}
 do
-    r=${r_seq[$z]}
-    for i in {0..14}
+    N=${N_seq[$v]}
+    for z in {0..5}
     do
-        k=${k_seq[$i]}
-        for j in {0..2}
-        do 
-            python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_days_${days}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --days=$days --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs   
+        r=${r_seq[$z]}
+        for i in {0..9}
+        do
+            k=${k_seq[$i]}
+            for j in {0..2}
+            do 
+                python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs   
+            done
+        done
+    done
+done
+
+# Optimizing FPs
+lambda_1=0.0
+lambda_2=1.0
+for v in {0..1}
+do
+    N=${N_seq[$v]}
+    for z in {0..5}
+    do
+        r=${r_seq[$z]}
+        for i in {0..9}
+        do
+            k=${k_seq[$i]}
+            for j in {0..2}
+            do 
+                python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs   
+            done
+        done
+    done
+done
+
+# Optimizing Tests
+lambda_1=0.0
+lambda_2=0.0
+for v in {0..1}
+do
+    N=${N_seq[$v]}
+    for z in {0..5}
+    do
+        r=${r_seq[$z]}
+        for i in {0..9}
+        do
+            k=${k_seq[$i]}
+            for j in {0..2}
+            do 
+                python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs   
+            done
         done
     done
 done
