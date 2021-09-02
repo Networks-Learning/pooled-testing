d1c0c75bda54c2ca267e883ac8e7266aa0e0ff4d
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-09-02 16:50:32 +0200

    1st experiment updated

--- scripts/score_vs_N.sh
+++ scripts/score_vs_N.sh
@@ -2,8 +2,9 @@
 
 N=(10 20 50 100 200 500)
 methods=(binomial negbin)
-se=0.95
-sp=0.95
+se=0.8
+sp=0.98
+d=0.0427
 r=2.5
 k=0.1
 seeds=100000
@@ -17,6 +18,6 @@ for i in {0..5}
 do
     for j in {0..1}
     do 
-        python -m src.experiment --output=outputs/score_vs_N_${methods[$j]}_N_${N[$i]}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --n=${N[$i]} --r=$r --k=$k --method=${methods[$j]} --seeds=$seeds --njobs=$njobs & 
+        python -m src.experiment --output=outputs/score_vs_N_${methods[$j]}_N_${N[$i]}_r_${r}_k_${k}_se_${se}_sp_${sp}_d_${d}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --d=$d --n=${N[$i]} --r=$r --k=$k --method=${methods[$j]} --seeds=$seeds --njobs=$njobs & 
     done
 done
