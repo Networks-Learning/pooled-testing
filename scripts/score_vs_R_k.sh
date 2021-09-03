48cbd9dfe3693303114153c9ebcc034ab3157373
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-09-03 14:49:05 +0200

    R-k experiment updated

--- scripts/score_vs_R_k.sh
+++ scripts/score_vs_R_k.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 methods=(binomial negbin)
-se=0.95
-sp=0.95
-N_seq=(20 100)
+se=0.8
+sp=0.98
+d=0.0427
+N_seq=(20 50 100 200)
 r_seq=(0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0)
 k_seq=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
 seeds=100000
@@ -13,7 +14,7 @@ njobs=1
 lambda_1=0.0
 lambda_2=0.0
 
-for v in {0..1}
+for v in {0..3}
 do
     N=${N_seq[$v]}
     for z in {0..9}
@@ -24,7 +25,7 @@ do
             k=${k_seq[$i]}
             for j in {0..1}
             do 
-                python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs  &
+                python -m src.experiment --output=outputs/score_vs_R_k_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_d_${d}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --d=$d --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs  &
             done
         done
         wait
