387df46de8a11f0b7d21eb36e6df56ac2e20ef46
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-09-06 15:46:20 +0200

    Lambdas experiment updated

--- scripts/lambdas.sh
+++ scripts/lambdas.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-N=100
-# se=0.9
-# sp=0.99
-# lambda_1_seq=$(seq 600 700)
-# lambda_2_seq=$(seq 0 3 300)
-# se=0.8
-# sp=0.98
-# lambda_1_seq=$(seq 0 300)
-# lambda_2_seq=$(seq 0 2 200)
+N=50
 se=0.7
 sp=0.97
-lambda_1_seq=$(seq 0 100)
-lambda_2_seq=$(seq 0 100)
+lambda_1_seq=$(seq 0 500)
+lambda_2_seq=$(seq 0 500)
 r=2.5
 k=0.1
 d=0.0427
-seeds=100000
+seeds=10000
 njobs=1
 
 for lambda_1 in $lambda_1_seq
