e01eb9acd7fd68b00631bfe7f3021bb4a10e8a8a
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-09-07 19:07:30 +0200

    minor fix

--- scripts/score_vs_d.sh
+++ scripts/score_vs_d.sh
@@ -19,6 +19,6 @@ do
     d=${d_seq[$z]}
     for j in {0..1}
     do 
-        python -m src.experiment --output=outputs/score_vs_d_N_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_d_${d}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --d=$d --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs  &
+        python -m src.experiment --output=outputs/score_vs_d_${methods[$j]}_N_${N}_r_${r}_k_${k}_se_${se}_sp_${sp}_d_${d}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --d=$d --n=$N --r=${r} --k=${k} --method=${methods[$j]} --seeds=$seeds --njobs=$njobs  &
     done
 done
