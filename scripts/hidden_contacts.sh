51a4d9f262b2cd54d6dc90bb27b94b15a695e07c
Stratis Tsirtsis <stsirtsis@mpi-sws.org>
2021-09-07 17:19:25 +0200

    Updated hidden contacts script

--- scripts/hidden_contacts.sh
+++ scripts/hidden_contacts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-N_seq=$(seq 20 50)
-untraced_seq=(0.0 0.1 0.2 0.3 0.4 0.5)
+# N_seq=$(seq 20 50)
+N_seq=(20 50 100 200)
+untraced_seq=(0.0 0.1 0.2 0.5)
 methods=(binomial negbin)
 se=0.8
 sp=0.98
@@ -15,15 +16,15 @@ njobs=1
 lambda_1=0.0
 lambda_2=0.0
 
-for i in {0..5}
+for i in {0..3}
 do
     untraced=${untraced_seq[$i]}
-    for N in $N_seq
+    for l in {0..3}
     do
+        N=${N_seq[$l]}
         for j in {0..1}
         do 
             python -m src.experiment --output=outputs/hidden_contacts_${methods[$j]}_N_${N}_untraced_${untraced}_r_${r}_k_${k}_se_${se}_sp_${sp}_d_${d}_l1_${lambda_1}_l2_${lambda_2}  --lambda_1=$lambda_1 --lambda_2=$lambda_2 --se=$se --sp=$sp --d=$d --n=$N --untraced=$untraced --r=$r --k=$k --method=${methods[$j]} --seeds=$seeds --njobs=$njobs & 
         done
     done
-    wait
 done
